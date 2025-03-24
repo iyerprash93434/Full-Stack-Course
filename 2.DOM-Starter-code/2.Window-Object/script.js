b2760362c12319a7f6fae56af8d7451e4af9275a
Prashant Iyer <E66493@sbilife.co.in>
2025-03-24 17:00:50 +0530

    changes added

---
+++ 2.DOM-Starter-code/2.Window-Object/script.js
@@ -0,0 +1,12 @@
+// window object
+console.log(window);
+console.log(window.document);
+console.log(window.innerHeight, window.innerWidth);
+console.log(window.location);
+console.log(window.history);
+console.log(window.navigator);
+
+console.log(window.screen);
+
+
+
---
+++ 2.DOM-Starter-code/3.getElementById/script.js
@@ -0,0 +1,12 @@
+let titleEl = document.getElementById("conferenceTitle");
+console.log(titleEl);
+
+let dateEl = document.getElementById("conferenceDate");
+console.log(dateEl);
+
+let listEl = document.getElementById("speakerList");
+console.log(listEl);
+
+
+let locationEl = document.getElementById("conferenceLocation");
+console.log(locationEl);
